17ff8d5a52e1fb1c71182e77be758b80f08edc0a
Fausto Bernal <z.fausto.a.b.b@gmail.com>
2025-12-02 15:45:21 -0700

    Primer commit del proyecto, hecho el layout

---
+++ app/src/main/java/com/example/contadordeono99/MainCalculator.kt
@@ -0,0 +1,22 @@
+package com.example.contadordeono99
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
+
+class MainCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_calculator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
